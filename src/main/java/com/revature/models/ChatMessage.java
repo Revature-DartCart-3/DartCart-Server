b5e9294f1c8138ad0c8498f4f9194d6b4aee7bd1
hrisse <72626109+hrisse@users.noreply.github.com>
2022-05-18 15:21:54 -0400

    Prototype backend for techChat

---
+++ src/main/java/com/revature/models/ChatMessage.java
@@ -0,0 +1,61 @@
+package com.revature.models;
+
+import javax.persistence.Id;
+
+public class ChatMessage {
+
+	
+    @Id
+    private String id;
+    private String chatId;
+    private String senderId;
+    private String recipientId;
+    private String senderName;
+    private String recipientName;
+    private MessageStatus status;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getChatId() {
+		return chatId;
+	}
+	public void setChatId(String chatId) {
+		this.chatId = chatId;
+	}
+	public String getSenderId() {
+		return senderId;
+	}
+	public void setSenderId(String senderId) {
+		this.senderId = senderId;
+	}
+	public String getRecipientId() {
+		return recipientId;
+	}
+	public void setRecipientId(String recipientId) {
+		this.recipientId = recipientId;
+	}
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	public String getRecipientName() {
+		return recipientName;
+	}
+	public void setRecipientName(String recipientName) {
+		this.recipientName = recipientName;
+	}
+	public MessageStatus getStatus() {
+		return status;
+	}
+	public void setStatus(MessageStatus status) {
+		this.status = status;
+	}
+	
+    
+    
+}
---
+++ src/main/java/com/revature/models/ChatRoom.java
@@ -0,0 +1,39 @@
+package com.revature.models;
+
+public class ChatRoom {
+    private String id;
+    private String chatId;
+    private String senderId;
+    private String recipientId;
+    
+    
+    
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getChatId() {
+		return chatId;
+	}
+	public void setChatId(String chatId) {
+		this.chatId = chatId;
+	}
+	public String getSenderId() {
+		return senderId;
+	}
+	public void setSenderId(String senderId) {
+		this.senderId = senderId;
+	}
+	public String getRecipientId() {
+		return recipientId;
+	}
+	public void setRecipientId(String recipientId) {
+		this.recipientId = recipientId;
+	}
+    
+    
+    
+}
