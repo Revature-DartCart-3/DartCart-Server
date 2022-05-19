b13d19e7ce85ab2079150693d3da204e87acb8e3
Stefan <67237726+StefWebDesigner@users.noreply.github.com>
2022-05-18 21:54:44 -0400

    commit before merging with hunters files

--- src/main/java/com/revature/controllers/ChatController.java
+++ src/main/java/com/revature/controllers/ChatController.java
@@ -10,13 +10,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
+    //TO ADD USERS TO THE CHAT
     @MessageMapping("/chat.register")
     @SendTo("/topic/public")
+    //AND THEN GET WHO GOING TO BE ACCESSED
     public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 
+    //TO CREATE A CONTENT
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
