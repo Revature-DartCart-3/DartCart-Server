485ceabea62e8675836f2ec2d89af9371a02ef19
hrisse <72626109+hrisse@users.noreply.github.com>
2022-05-19 16:39:42 -0400

    test tech chaqt

--- src/main/java/com/revature/configs/SocketBrokerConfig.java
+++ src/main/java/com/revature/configs/SocketBrokerConfig.java
@@ -1,9 +1,12 @@
 package com.revature.configs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+<<<<<<< Updated upstream
 @SuppressWarnings("deprecation")
 public class SocketBrokerConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	
@@ -20,3 +23,28 @@ public class SocketBrokerConfig extends AbstractWebSocketMessageBrokerConfigurer
 	}
 	
 }
+=======
+//@SuppressWarnings("deprecation")
+
+//public class SocketBrokerConfig extends AbstractWebSocketMessageBrokerConfigurer {
+@Configuration
+@EnableWebSocketMessageBroker
+public class SocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker (MessageBrokerRegistry registry) {
+//        config.enableStompBrokerRelay("/techchat/queue/specific-user");
+//        config.setApplicationDestinationPrefixes("/spring-security-mvc-socket");
+//        config.setUserDestinationPrefix("/techchat/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/chatroom","/user");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //registry.addEndpoint("/ws").withSockJS();
+    	registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+}
+>>>>>>> Stashed changes
